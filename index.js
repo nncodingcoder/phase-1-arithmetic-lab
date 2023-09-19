11c4ed6b67fb20a505c091f9358512edbda7b989
naomiinyele <inyeleinyele@gmail.com>
2023-09-19 03:55:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+    let num1 = 31
+    let num2 = 2
+    const multiply = num1 * num2
+    multiply;
+
+    const random = Math.floor(Math.random() * 10) + 1
+
+
+    let num3 = 10
+    let num4 = 6
+    const mod = num3 % num4
+    mod
+
+    const max = Math.max(20, 10, 5)
+    max
